11ad38c245e0f785cac4e3df36262a288600cbbb
Lenard-Mate <57682558+Lenard-Mate@users.noreply.github.com>
2025-05-27 00:40:32 +0300

    feature/JAR-12: Add theme toggle, Prism.js integration, and Bootstrap Icons
    Introduced a theme toggle functionality with a "changeTheme" function and updated UI controls for improved interactivity. Added Prism.js for syntax highlighting and Bootstrap Icons for enhanced visuals. Updated project dependencies and static asset handling in the server to support these changes.

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const PORT = 3000;
 
 app.use(express.json());
 app.use('/bootstrap', express.static(path.join(__dirname, 'node_modules/bootstrap/dist')));
+app.use('/icons', express.static(path.join(__dirname, 'node_modules/bootstrap-icons/font')));
+app.use('/prismjs', express.static(path.join(__dirname, 'node_modules/prismjs')));
+
 app.use(express.static(path.join(__dirname, 'frontend')));
 app.use('/api', dispatcher);
 
